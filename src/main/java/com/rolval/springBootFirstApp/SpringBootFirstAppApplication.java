2548d17898f9b9ee7e5e337908ea0af4f1327244
Rolando Valdez <rvaldez@ippbg.com.mx>
2020-02-17 13:09:06 -0600

    Created User and Roles DDL

--- src/main/java/com/rolval/springBootFirstApp/SpringBootFirstAppApplication.java
+++ src/main/java/com/rolval/springBootFirstApp/SpringBootFirstAppApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootFirstAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstAppApplication.class, args);
-		
+		SpringApplication.run(SpringBootFirstAppApplication.class, args);	
 	}
 
 }
